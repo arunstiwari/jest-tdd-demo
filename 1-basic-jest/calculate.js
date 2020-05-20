5630d29d60e946f1c26f4cf3bec5a82f7f830346
ARUN SHANKAR TIWARI <arunstiwari@gmail.com>
2020-05-20 21:50:37 +0530

    Update calculate.js
    Refactored the code to support the Exception Scenarios

--- 1-basic-jest/calculate.js
+++ 1-basic-jest/calculate.js
@@ -1,7 +1,18 @@
-exports.sum = (a, b) => {
-    return a+b;
+class InvalidArgumentError extends Error {}
+
+const sum = (a, b) => {
+    if (validateArgument(a,b)) {
+        return a + b;
+    }
+    throw new InvalidArgumentError('Argument should be number');
 }
 
-exports.subtract = (a,b) => {
+const validateArgument = (a,b) => {
+    return (parseInt(a) && parseInt(b));
+}
+
+const subtract = (a,b) => {
     return a-b;
 }
+
+module.exports = {sum, subtract, InvalidArgumentError}
