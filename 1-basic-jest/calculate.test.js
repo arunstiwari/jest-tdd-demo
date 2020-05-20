06dc315cc81f5cf843a217effe56f41047f27dd4
ARUN SHANKAR TIWARI <arunstiwari@gmail.com>
2020-05-20 21:47:27 +0530

    Update calculate.test.js
    Added the test for error scenario

--- 1-basic-jest/calculate.test.js
+++ 1-basic-jest/calculate.test.js
@@ -1,4 +1,4 @@
-const {sum, subtract} = require('./calculate');
+const {sum, InvalidArgumentError, subtract} = require('./calculate');
 
 test('2 +3 = 5', () => {
     const result = sum(2,3);
@@ -9,6 +9,36 @@ test('3+5=8', () => {
     const result = sum(3,5);
     expect(result).toBe(8);
 })
+
+test("2 + c  throw an exception", () => {
+    try {
+        const result = sum(2, "c"); 
+    } catch (e) {
+        let isExpectedError = e instanceof InvalidArgumentError;
+        console.log('===isExpectedError---',isExpectedError);
+        expect(isExpectedError).toBeTruthy();
+        expect(e.message).toBe('Argument should be number');
+    }
+});
+
+test("a + c  throw an exception", () => {
+  try {
+    const result = sum("a", "c");
+  } catch (e) {
+      let isExpectedError = e instanceof InvalidArgumentError;
+      console.log("===isExpectedError---", isExpectedError);
+      expect(isExpectedError).toBeTruthy();
+    expect(e.message).toBe("Argument should be number");
+  }
+});
+
+test("a + 5  throw an exception", () => {
+  try {
+    const result = sum("a", 5);
+  } catch (e) {
+    expect(e.message).toBe("Argument should be number");
+  }
+});
 
 test('5-2=3', () => {
     const result = subtract(5,2);
